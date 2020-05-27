b39b5842fd6dba68d00786f48b2c835a68b0919c
Luis Marin <tsuluismarin@gmail.com>
2020-05-27 17:39:58 -0300

    Creamos login + registro sencillo

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var inicioController = require('../controllers/inicioController.js')
+// var inicioController = require('../controllers/inicioController.js')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', inicioController.inicio);
+  res.render('index', { title: "Inicio"});
 });
 
 module.exports = router;
